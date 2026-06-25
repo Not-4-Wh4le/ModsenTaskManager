210cd37a5956cc2bfe861d7c30bf34a0344dbc49
Nikita <notawh4le@gmail.com>
2026-06-25 04:33:04 +0300

    feat(aplication) add custom error in Result pattern

--- ResultPattern/Error.cs
+++ ResultPattern/Error.cs
@@ -7,5 +7,7 @@ namespace ResultPattern
     public record Error(string Code, string Descrition)
     {
         public static readonly Error None = new(string.Empty, string.Empty);
+
+        public static Error Custom(string code, string description) => new(code, description);
     }
 }
