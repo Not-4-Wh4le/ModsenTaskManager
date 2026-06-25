4b9cb184f6a8e804286015c11371b079f601d2d3
notawhale <notawh4le@gmail.com>
2026-06-25 12:37:04 +0300

    feat(infrastructure): add jwt service

---
+++ Infrastructure/JwtService/JwtOptions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.JwtService
+{
+    public record JwtOptions()
+    {
+        public string Issuer { get; init; } = string.Empty;
+        public string SecretKey { get; init; } = string.Empty;
+        public string Audience { get; init; } = string.Empty;
+        public int TokenLifetimeInMinutes { get; init; }
+        public SymmetricSecurityKey GetSymmetricSecurityKey()
+            => new SymmetricSecurityKey(Encoding.UTF8.GetBytes(SecretKey));
+    }
+}
---
+++ Infrastructure/JwtService/JwtService.cs
@@ -0,0 +1,37 @@
+﻿using Application.Common.Interfaces;
+using Domain;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Infrastructure.JwtService
+{
+    public class JwtService(IOptions<JwtOptions> options) : ITokenService
+    {
+        private readonly JwtOptions jwtOpt = options.Value;
+        public string GenerateToken(User user)
+        {
+            var claims = new List<Claim> 
+            { 
+                new (ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new (ClaimTypes.Email, user.Email)
+            };
+
+            var jwt = new JwtSecurityToken(
+                issuer: jwtOpt.Issuer,
+                audience: jwtOpt.Audience,
+                expires: DateTime.UtcNow.Add(TimeSpan.FromMinutes(jwtOpt.TokenLifetimeInMinutes)),
+                claims: claims,
+                signingCredentials: 
+                    new SigningCredentials(jwtOpt.GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256));
+
+            var token = new JwtSecurityTokenHandler().WriteToken(jwt);
+
+            return token;
+        }
+    }
+}
