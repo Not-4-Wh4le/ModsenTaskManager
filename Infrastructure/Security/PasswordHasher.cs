c7bf1d60598ec11250e0e86a72f00ef83e955f53
Nikita <notawh4le@gmail.com>
2026-06-28 23:52:14 +0300

    feat(infrastructure): add password hasher implementation

---
+++ Infrastructure/Security/PasswordHasher.cs
@@ -0,0 +1,20 @@
+﻿using Application.Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Security
+{
+    public class PasswordHasher : IPasswordHasher
+    {
+        public string HashPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+        public bool VerifyPassword(string password, string passwordHash)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, passwordHash);
+        }
+    }
+}
