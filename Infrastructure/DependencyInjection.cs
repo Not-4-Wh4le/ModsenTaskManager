cfc9241ea51a8cfcf9426fbe11344b9cc8f41977
Nikita <notawh4le@gmail.com>
2026-06-28 23:54:25 +0300

    feat(infrastructure): add DI

---
+++ Infrastructure/DependencyInjection.cs
@@ -0,0 +1,31 @@
+﻿using Application.Common.Interfaces;
+using Application.Common.Interfaces.Repositories;
+using Infrastructure.Persistence;
+using Infrastructure.Persistence.Repositories;
+using Infrastructure.Security;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection services)
+        {
+            MongoConfiguration.Configure();
+            services.AddScoped<MongoDbContext>();
+
+            services.AddSingleton<IPasswordHasher, PasswordHasher>();
+
+            services.AddScoped<IUserRepository, MongoUserRepository>();
+            services.AddScoped<IProjectRepository, MongoProjectRepository>();
+            services.AddScoped<ITaskRepository, MongoTaskRepository>();
+
+            services.AddSingleton<ITokenService, TokenService.JwtService>();
+
+            return services;
+        }
+    }
+}
