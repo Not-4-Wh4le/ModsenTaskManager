0fcf73f9b4212b4b32b7ba0677718094192b2d1e
Nikita <notawh4le@gmail.com>
2026-06-29 20:36:58 +0300

    fix(application): fix a typo in Error record

--- ResultPattern/Error.cs
+++ ResultPattern/Error.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace ResultPattern
 {
-    public record Error(string Code, string Descrition)
+    public record Error(string Code, string Description)
     {
         public static readonly Error None = new(string.Empty, string.Empty);
 
