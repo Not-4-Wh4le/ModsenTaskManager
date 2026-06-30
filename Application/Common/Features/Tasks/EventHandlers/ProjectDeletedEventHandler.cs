540555e895d66fd970bbd5aa5d94ef42b3efc1ef
Nikita <notawh4le@gmail.com>
2026-06-30 20:06:46 +0300

    feat(application): add tasks event handlers

---
+++ Application/Common/Features/Tasks/EventHandlers/ProjectDeletedEventHandler.cs
@@ -0,0 +1,35 @@
+﻿using Application.Common.Interfaces.Repositories;
+using Domain.Events;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Common.Features.Tasks.EventHandlers
+{
+    public class ProjectDeletedEventHandler : INotificationHandler<ProjectDeletedEvent>
+    {
+        private readonly ITaskRepository _taskRepository;
+        private readonly ILogger<ProjectDeletedEventHandler> _logger;
+        public ProjectDeletedEventHandler(
+            ITaskRepository taskRepository,
+            ILogger<ProjectDeletedEventHandler> logger)
+        {
+            _taskRepository = taskRepository;
+            _logger = logger;
+        }
+
+        public async Task Handle(ProjectDeletedEvent notification, CancellationToken cancellationToken)
+        {
+            _logger.LogInformation(
+                "Timestamp: [{Timestamp}]\nProject [{ProjectName}] (Id: {ProjectId}) has been deleted by user with id [{UserId}]",
+                notification.Timestamp,
+                notification.ProjectName,
+                notification.ProjectId,
+                notification.UserId);
+
+            await _taskRepository.DeleteAllByProjectIdAsync(notification.ProjectId, cancellationToken);
+        }
+    }
+}
---
+++ Application/Common/Features/Tasks/EventHandlers/TaskDeletedEventHandler.cs
@@ -0,0 +1,31 @@
+﻿using Application.Common.Interfaces.Repositories;
+using Domain.Events;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Common.Features.Tasks.EventHandlers
+{
+    internal class TaskDeletedEventHandler : INotificationHandler<TaskDeletedEvent>
+    {
+        private readonly ILogger<TaskDeletedEventHandler> _logger;
+        
+        public TaskDeletedEventHandler(ILogger<TaskDeletedEventHandler> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task Handle(TaskDeletedEvent notification, CancellationToken cancellationToken)
+        {
+            _logger.LogInformation(
+                 "Timestamp: [{Timestamp}]\nProject [{Title}] (Id: {ProjectId}) has been deleted",
+                 notification.Timestamp,
+                 notification.Title,
+                 notification.ProjectId);
+
+            return Task.CompletedTask;
+        }
+    }
+}
